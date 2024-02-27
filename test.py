7b1c7cc9f9da1e31333d07628b785b7f3994f64b
winsonc7 <winsonc@stanford.edu>
2024-02-26 21:39:45 -0800

    added scikit nn

---
+++ test.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report
+
+print("Hello world!")
